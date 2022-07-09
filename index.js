e6d06b4c215c1b73229bb970490b6c570bc9d782
daniel.reyt <daniel-david.reyt@uha.fr>
2022-07-09 16:35:25 +0200

    reset token

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ bot.on("ready", function () {
 })
 
 
-bot.on("message", function (mess) {
+bot.on("messageCreate", function (mess) {
     if (mess.content.startsWith("~clear")) {
         mess.delete()
         mess.channel.bulkDelete(100)
